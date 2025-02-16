4dfa7014f6327d76bbde49212531f779e1ba0c53
Kalmarr-dev <art.hydrogen1405@gmail.com>
2025-02-16 14:39:36 +0200

    feature: basic backend structure

--- joke-backend/src/app.module.ts
+++ joke-backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { JokeController } from './joke/joke.controller';
+import { JokeService } from './joke/joke.service';
+import { JokeModule } from './joke/joke.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [JokeModule],
+  controllers: [AppController, JokeController],
+  providers: [AppService, JokeService],
 })
 export class AppModule {}
